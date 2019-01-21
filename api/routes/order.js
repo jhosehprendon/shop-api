a6d90ab06a06f9ba36884441d285faa75a3a29eb
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-01-21 13:10:31 -0500

    Handling errors

---
+++ api/routes/order.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Order was created'
+    })
+})
+
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order details',
+        id: req.params.orderId
+    })
+})
+
+router.delete('/:orderId', (req, res, next) => {
+    const id = req.params.orderId
+    res.status(200).json({
+        message: `Order ${id} deleted`
+    })
+})
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+const productRoutes = require('./api/routes/products')
+const orderRoutes = require('./api/routes/order')
+
+app.use(morgan('dev'))
+
+app.use('/products', productRoutes)
+app.use('/orders', orderRoutes)
 
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works'
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.staus || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
     })
 })
 
