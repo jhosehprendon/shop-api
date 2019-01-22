3170c407099bf9b10262a0afce24b3bcbfbe051a
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-01-22 10:59:51 -0500

    Added JWT route protection

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const checkAuth = require('../middleware/check-auth');
 
 const Order = require('../models/order');
 const Product = require('../models/product');
 
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
     Order.find().select('product quantity _id').populate('product', 'name').exec().then(docs => {
         res.status(200).json({
             count: docs.length,
@@ -29,7 +30,7 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
 
     Product.findById(req.body.productId).then(product => {
         if(!product) {
@@ -64,7 +65,7 @@ router.post('/', (req, res, next) => {
     }) 
 })
 
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', checkAuth, (req, res, next) => {
     Order.findById(req.params.orderId).populate('product').exec().then(order => {
         if(!order) {
             return res.status(404).json({
@@ -85,7 +86,7 @@ router.get('/:orderId', (req, res, next) => {
     })
 })
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
     Order.remove({_id: req.params.orderId}).exec().then(result => {
         res.status(200).json({
             message: 'Order deleted'
--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer =  require('multer');
+const checkAuth = require('../middleware/check-auth');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -59,7 +60,7 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.post('/', upload.single('productImage'), (req, res, next) => {
+router.post('/', checkAuth, upload.single('productImage'), (req, res, next) => {
 
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
@@ -118,7 +119,7 @@ router.get('/:productId', (req, res, next) => {
     
 })
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
     const updateOps = {}
 
     for(const ops of req.body ) {
@@ -142,7 +143,7 @@ router.patch('/:productId', (req, res, next) => {
     })
 })
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
     
     Product.remove({ _id: req.params.productId }).exec().then(result => {
         res.status(200).json({
