627e0127346cc8c4a466e6c5b8ee92b14acca89f
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-01-22 09:41:15 -0500

    Added user model, signup and delete user routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+const userRoutes = require('./api/routes/user')
 
 mongoose.connect('mongodb://shop-node:' + process.env.MONGO_ATLAS_PW + '@node-shop-app-shard-00-00-cweaz.mongodb.net:27017,node-shop-app-shard-00-01-cweaz.mongodb.net:27017,node-shop-app-shard-00-02-cweaz.mongodb.net:27017/test?ssl=true&replicaSet=node-shop-app-shard-0&authSource=admin&retryWrites=true', {
     useMongoClient: true
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
+app.use('/user', userRoutes)
 
 app.use((req, res, next) => {
     const error = new Error('Not found')
